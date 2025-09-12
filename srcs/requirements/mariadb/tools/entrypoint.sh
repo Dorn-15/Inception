8c060f9f0c13d4f030b62029db06f4f89c765930
Dorn <ddoireauarthur@gmail.com>
2025-09-12 17:46:37 +0200

    fix db

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 set -e
 
+DB_PATH="/var/lib/mysql"
 DB_NAME="${MYSQL_DATABASE}"
 DB_USER="${MYSQL_USER}"
 DB_PASS="$(cat /run/secrets/db_password.txt)"
 DB_ROOT_PASS="$(cat /run/secrets/db_root_password.txt)"
 
-if [ ! -d /var/lib/mysql/mysql ]; then
-	mariadb-install-db --user=mysql --ldata=/var/lib/mysql > /dev/null
-
-	# Démarrage temporaire sans réseau externe
-	mysqld --skip-networking &
+if [ ! -d ${DB_PATH}/mysql ]; then
+	mariadb-install-db --user=mysql --ldata=${DB_PATH} > /dev/null
+	mariadbd --user=mysql --skip-networking &
 	pid="$!"
 	sleep 5
 
 	mariadb -uroot <<-SQL
 		ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+		CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASS}';
+		GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 		DELETE FROM mysql.user WHERE User='';
 		FLUSH PRIVILEGES;
 SQL
@@ -27,9 +28,8 @@ SQL
 		FLUSH PRIVILEGES;
 SQL
 
-	# Arrêt propre du démarrage temporaire
 	mysqladmin -uroot -p"${DB_ROOT_PASS}" shutdown
 	wait "$pid" || true
 fi
 
-exec mysqld
+exec mariadbd --user=mysql --bind-address=0.0.0.0 --port=3306 --skip-networking=0
